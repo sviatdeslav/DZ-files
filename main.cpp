66f2ac837c1c3ffbf00ab7cd711722634c2dc963
sviatdeslav <125045110+sviatdeslav@users.noreply.github.com>
2023-04-01 23:45:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,46 @@ using namespace std;
 Реализовать чтение заголовков файлов форматов
 PNG: сигнатура и IHDR Chunk*/
 
+int main(){
+    std::ifstream png;
+    png.open("sevgu1.PNG",std::ios::in);
+    if (png.is_open()) {
+        #pragma pack(push,1)//задает текущее значение выравнивания упаковки на 1 байт
+        struct Png_str{
+            unsigned int length;//длина блока
+            unsigned int type;//тип блока
+            unsigned int width;//ширина изображения
+            unsigned int height;//высота изображения
+            unsigned char depth;//глубина
+            unsigned char color;//тип цвета
+            unsigned char compression;//метод сжатия
+            unsigned char filter;//фильтр
+            unsigned char interlace;//черезстрочность
+            unsigned int crc;//"контрольная" сумма
+        };
+        #pragma pack(pop);//удаляет запись из верхней части области временного хранения данных компилятора
+        Png_str object;//объект
+        unsigned char sign[8];//сигнатура
+        png.read((char*)&sign,8);
+        png.read((char*)&object,sizeof(object));
+        png.close();
+        cout<<"Signature(hex): "<<showbase<<hex
+           <<+sign[0]<<" "<<+sign[1]<<" "<<+sign[2]<<" "<<+sign[3]<<" "<<+sign[4]<<" "
+           <<+sign[5]<<" "<<+sign[6]<<" "<<+sign[7]<<" "<<endl;
+        cout<<"Chunk IHDR("<<dec<<sizeof(object)<<" bytes)"<<endl;
+        cout<<"length "<<object.length<<" (true = "<<__builtin_bswap32(object.length)<<")"<<endl;
+        cout<<"type "<<object.type<<" [(char) "<<(char)(object.type)<<(char)(object.type>>8)
+           <<(char)(object.type>>16)<<(char)(object.type>>24)<<"]"<<endl;
+        cout<<"width "<<(int)object.width<<" (true = "<<__builtin_bswap32(object.width)<<")"<<endl;
+        cout<<"height "<<(int)object.height<<" (true = "<<__builtin_bswap32(object.height)<<")"<<endl;
+        cout<<"depth "<<(int)object.depth<<endl;
+        cout<<"color "<<(int)object.color<<endl;
+        cout<< "compression "<<(int)object.compression<<endl;
+        cout<< "filter "<<(int)object.filter<<endl;
+        cout<< "interl "<<(int)object.interlace<<endl;
+        cout<<"crc "<<(int)object.crc;
+    }
+}
 
 
 /*Задача 3(С)
@@ -18,7 +58,7 @@ PNG: сигнатура и IHDR Chunk*/
 считывать из файла in.txt. Результат записывать в текстовый файл out.txt
 y=1/2x+1/4x+...+1/Чx*/
 
-/*float func(float a, int b){
+float func(float a, int b){
     float c=0;
     int n=2;
     while(n<=b){
@@ -45,7 +85,6 @@ int main(){
     }
     return 0;
 }
-*/
 
 /*Задача 6
 Разработать программу для считывания полётных данных о квадрокоптерах из
